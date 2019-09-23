409bfa5406fc0089435d82bfd27b95497cca31f1
Kay Wood <kwood15@users.noreply.github.com>
2019-09-23 15:01:18 +0100

    Add missing types and fix ref issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,46 @@
-import * as React from "react";
-import { FilePond, registerPlugin } from "react-filepond";
+import React, { useRef } from 'react';
+import { FilePond, FileProps, registerPlugin } from 'react-filepond';
 
-import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+// @ts-ignore - todo declare module 
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
 
-import FilePondPluginImagePreview from "filepond-plugin-image-preview";
-import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+// @ts-ignore
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
-export interface File {
+export interface File extends FileProps {
   source: string;
   options: {
     type: string;
   };
 }
 
-export type FileUploadProps = { pond };
+export type FileUploadProps = { 
+  pond: string
+};
 
-export function FileUpload({ pond }: FileUploadProps) {
+export function FileUpload() {
   const [files, setFiles] = React.useState<File[]>([]);
 
   const handleInit = () => {
-    console.log("testing", pond);
+    console.log('testing');
   };
+
+  const pondRef = useRef(null);
+
   return (
     <FilePond
-      // ref={ref => (pond = ref)} // useRef?
-      files={files}
+      ref={pondRef}
+      // files={files}
       allowMultiple={true}
       maxFiles={3}
       server="/api"
       oninit={() => handleInit()}
-      onupdatefiles={(fileItems: File[]) =>
-        setFiles(fileItems.map((fileItem: File) => fileItem.file))
-      }
+      // onupdatefiles={(fileItems: File[]) =>
+      //   setFiles(fileItems.map((fileItem: File) => fileItem.file))
+      // }
     />
   );
 }
